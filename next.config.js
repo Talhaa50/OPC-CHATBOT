db65e2a2e9451211c006d52991407261930e4ede
Talha Fiaz <talhahjutt50@gmail.com>
2026-05-12 11:39:44 +0500

    Update security headers in next.config.js

--- next.config.js
+++ next.config.js
@@ -6,8 +6,24 @@ const nextConfig = {
         source: '/(.*)',
         headers: [
           {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL',
+          },
+          {
             key: 'Content-Security-Policy',
-            value: "frame-ancestors * https: http:",
+            value: "frame-ancestors 'self' https: http:",
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, x-admin-password',
           },
         ],
       },
